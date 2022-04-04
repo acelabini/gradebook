13eda8cd7d0257949e59e2fddd1c44cb997dbc03
Ace <c.labini@salarium.com>
2022-04-04 10:06:13 +0800

    Added show template button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 
     <style>
         body {
@@ -34,7 +35,7 @@
                     <label class="form-label">Input grades</label>
                     <textarea class="form-control{{ $errors->has('grades') ? ' is-invalid' : '' }}" name="grades" rows="10"></textarea>
                 </div>
-                <div>OR</div>
+                <h2>OR</h2>
                 @if ($errors->has('grades_file'))
                     <div class="invalid-feedback">
                         <strong>{{ $errors->first('grades_file') }}</strong>
@@ -48,6 +49,7 @@
                     </div>
                 </div>
                 <div class="mt-3">
+                    <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#templateModal" type="button">Show Template</button>
                     <button class="btn btn-primary" type="submit">Submit</button>
                 </div>
             </form>
@@ -83,6 +85,20 @@
                     </tbody>
                 @endforeach
             </table>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="templateModal" tabindex="-1" aria-labelledby="templateModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" style="max-width: 550px;">
+        <div class="modal-content">
+            <div class="modal-body">
+                Quarter 1, 2019<br/>
+                John Wright H 86 55 96 78 T 82 89 93 70 74 H 93 85 80 74 76 82 62<br/>
+                Susan Smith H 75 88 94 95 84 68 91 74 100 82 93 T 73 82 81 92 85<br/>
+                Jane Jones T 88 94 100 82 95 H 84 66 74 98 92 85 100 95 96 42 88<br/>
+                Jimmy Doe H 73 99 98 83 85 92 100 60 74 98 92 T 84 96 79 91 95<br/>
+                Suzy Johnson H 65 72 78 80 82 74 76 0 85 75 76 T 74 79 70 83 78<br/>
+            </div>
         </div>
     </div>
 </div>
